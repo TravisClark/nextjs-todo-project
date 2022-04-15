67d58019170caf1dc8205b0298ababbcf455a05a
197PM31219 - TÔ MINH TRIẾT - K25T-PM1 <triet.197pm31219@vanlanguni.vn>
2022-04-15 12:13:58 +0700

    Login/ Register completed

---
+++ store/auth-actions.js
@@ -0,0 +1,29 @@
+import { authActions } from "./auth-slice";
+import { uiActions } from "./ui-slice";
+
+const api_Key = 'AIzaSyAAed619AkO9nmI95BDSl1MZ321QoOiGBA';
+const signUpUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${api_Key}`
+const signInUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${api_Key}`
+
+export const authRequest = (authData) => {
+    return async (dispatch) => {
+        const sendRequest = async () => {
+            const response = await fetch(authData.req === 'login' ? signInUrl : signUpUrl,{
+                method: 'POST',
+                body: JSON.stringify({email: authData.email, password: authData.password, returnSecureToken: false}),
+                headers: { 'Content-Type': 'application/json'}
+            })
+            if(!response.ok) {
+                dispatch(uiActions.wrongPwWarningNotification(true))
+                throw new Error("Authentication failed")
+            }
+        }
+        try {
+            await sendRequest();
+            authData.req === 'login' && dispatch(authActions.loginHandler(authData));
+            console.log('Authentication successful')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
--- store/auth-slice.js
+++ store/auth-slice.js
@@ -1,12 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState: { isLoggedIn: false },
-    reducers:{
-        
+  name: "auth",
+  initialState: {
+    isLoggedIn: false,
+    email: "",
+    password: "",
+    confirmPassword: "",
+    warningMessage: "",
+    validation: {pwChecked: true},
+  },
+  reducers: {
+    registerPwChecked(state, action) {
+      action.payload.password !== action.payload.confirmPassword
+        ? (state.pwChecked = false)
+        : (state.pwChecked = true);
     },
-})
+    loginHandler(state, action) {
+      localStorage.setItem('accountData', JSON.stringify(action.payload));
+      state.isLoggedIn = true;
+
+    }
+  },
+});
 
 export const authActions = authSlice.actions;
 export default authSlice.reducer;
